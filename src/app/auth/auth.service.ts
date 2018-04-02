429325c47ddd33804b745d0ee2913a540c6ccdc4
nickdunn2 <nick.dunn@flosports.tv>
2018-04-01 22:02:53 -0500

    WIP: AuthService improvements

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/operator/map'
 import { JwtHelper } from 'angular2-jwt'
 
 import { CookieService } from '../shared/services/cookie.service'
@@ -32,20 +31,40 @@ export interface ITokenResponse {
 
 @Injectable()
 export class AuthService {
+  private TOKEN_STORAGE_KEY = 'jwt_token'
   private jwtHelper = new JwtHelper()
 
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   public generateToken(email: string, password: string): Observable<ITokenResponse> {
+    // TODO: Can't have hard-coded staging endpoint
     return this.http.post<ITokenResponse>('https://staging-api.flosports.tv/api/tokens', { email, password })
   }
 
   public setTokenRaw(rawToken: string) {
-    this.cookieService.set('jwt_token', rawToken, {
+    this.cookieService.set(this.TOKEN_STORAGE_KEY, rawToken, {
       secure: false,
       path: '/',
       expires: this.getTokenExpirationDate(rawToken)
     })
+  }
+
+  public getValidToken(token: string | undefined): any {
+    if (!token) {
+      return undefined
+    }
+    try {
+      const decodedToken = this.jwtHelper.decodeToken(token)
+      return decodedToken && !this.jwtHelper.isTokenExpired(token)
+        ? decodedToken
+        : undefined
+    } catch (e) {
+      return undefined
+    }
+  }
+
+  public getTokenFromStore(): string {
+    return this.cookieService.get(this.TOKEN_STORAGE_KEY)
   }
 
   private getTokenExpirationDate(rawToken: string): Date {
