d426fcfb7a43f4e860c13139323f43f211a847f3
nickdunn2 <nick.dunn@flosports.tv>
2018-04-03 21:57:42 -0500

    WIP: add hacky version of UsersGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './auth/login/login.component'
 import { UsersComponent } from './users/users.component'
+import { UsersGuard } from './users/users-guard.service'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent, canActivate: [ UsersGuard ] }
 ]
 
 @NgModule({
