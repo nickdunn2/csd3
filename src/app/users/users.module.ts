cd5b05e17dbb7e5a174c685ea75d1acdf7ecaaf2
nickdunn2 <nick.dunn@flosports.tv>
2018-04-03 21:20:08 -0500

    refactor: introduce SharedModule

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { ReactiveFormsModule } from '@angular/forms'
 
-
-import { MaterialModule } from '../material.module'
 import { UsersComponent } from './users.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [
     UsersComponent
   ],
   imports: [
-    CommonModule,
-    MaterialModule,
-    FlexLayoutModule,
-    ReactiveFormsModule
+    SharedModule
   ]
 })
 export class UsersModule {
